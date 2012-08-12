65891feffd9d74fde89542862bc6602e00e5bbc0
Postmodern <postmodern.mod3@gmail.com>
2012-08-11 20:05:41 -0700

    Moved FFI stuff into library.rb.

---
+++ lib/ffi/http/parser/library.rb
@@ -0,0 +1,17 @@
+require 'ffi/http/parser/types'
+
+module FFI
+  module HTTP
+    module Parser
+      extend FFI::Library
+
+      ffi_lib ['http_parser', 'http_parser.so.1']
+
+      attach_function :http_parser_init, [:pointer, :http_parser_type], :void
+      attach_function :http_parser_execute, [:pointer, :pointer, :pointer, :size_t], :size_t
+
+      attach_function :http_should_keep_alive, [:pointer], :int
+      attach_function :http_method_str, [:http_method], :string
+    end
+  end
+end
